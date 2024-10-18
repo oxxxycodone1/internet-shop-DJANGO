60adb9344fd8db55f0d5383e4be0f65116a419e8
oxxxycodone1 <dorozhansky2@gmail.com>
2024-10-18 12:36:11 +0300

    Delete static/js directory

--- static/js/admin_custom.js
+++
@@ -1,9 +0,0 @@
-// Пример скрипта для улучшения пользовательского интерфейса
-document.addEventListener('DOMContentLoaded', function() {
-    const rows = document.querySelectorAll('table tbody tr');
-    rows.forEach(row => {
-        row.addEventListener('click', () => {
-            window.location = row.querySelector('a').href; // Переход по клику на строку
-        });
-    });
-});
